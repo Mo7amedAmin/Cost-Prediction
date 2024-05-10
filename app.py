b488200bec0e47f3ca10e01cb143238f1a1d836d
Mohamed Ali Amin <mohamedali18210216@gmail.com>
2024-05-10 14:52:44 +0300

    handle content type

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 
-# Sample regression model (replace this with your trained model)
 model = pickle.load(open("model.pkl", "rb"))
-#preprocessor = pickle.load(open("preprocessing.pkl", "rb"))
 
 @app.route('/')
 def home():
@@ -18,24 +15,13 @@ def predict():
     input_data = request
 
     if input_data.is_json:
-        input_data = input_data.json
-        #print(input_data)
-        #input_data.form.to_dict()
-        input_data['Children'] = int(input_data['Children'])
-        input_data['Additional Features Number'] = int(input_data['Additional Features Number'])
-
-        for column in ['Store Sales', 'Net Weight', 'Package Weight', 'Store Area', 'Frozen Area']:
-            input_data[column] = float(input_data[column])
-
-        
-        #print(np.array(list(input_data.values()))).reshape(1, -1)
-        input_data = np.array(list(input_data.values())).reshape(1, -1)
-        prediction = model.predict(input_data)
-        return jsonify({'prediction': prediction[0]})
-    
+        input_data = input_data.get_json(force=True)
     else:
-        return jsonify({'error': 'there is no entered data'})
-    
+        input_data = input_data.form.to_dict()
 
+    input_data = np.array(list(input_data.values())).reshape(1, -1)
+    prediction = model.predict(input_data)
+    return jsonify({'prediction': prediction[0]})
+    
 if __name__ == '__main__':
     app.run(debug=True)
